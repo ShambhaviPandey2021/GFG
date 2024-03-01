90ec2766337489d6729b4dbadfc44653576bd55e
Shambhavi Pandey <112650493+ShambhaviPandey2021@users.noreply.github.com>
2024-03-01 22:38:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Check if a number is divisible by 8/check-if-a-number-is-divisible-by-8.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+    def DivisibleByEight(self,s):
+        #code here
+        n =len(s)
+        if n<3:
+            if int(s)%8==0:
+                return 1
+            else :
+                return -1
+        x =s[-3:]
+        if int(x)%8==0:
+            return 1
+        else:
+            return -1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        S=input()
+        ob=Solution()
+        print(ob.DivisibleByEight(S))
+# } Driver Code Ends
