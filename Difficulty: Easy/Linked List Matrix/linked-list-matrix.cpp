bbfe6e4c9ff57ab066118241e54c7383acfb944c
Shambhavi Pandey <112650493+ShambhaviPandey2021@users.noreply.github.com>
2024-10-11 23:05:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Linked List Matrix/linked-list-matrix.cpp
+++ Difficulty: Easy/Linked List Matrix/linked-list-matrix.cpp
@@ -36,7 +36,6 @@ class Solution {
   public:
     Node* constructLinkedMatrix(vector<vector<int>>& mat) {
         // code Here
-
         int n=mat.size();
         if(n==0) return NULL;
         vector<vector<Node*>>Nodemat(n,vector<Node*>(n,NULL));
@@ -51,11 +50,11 @@ class Solution {
                if(j<n-1) Nodemat[i][j]->right=Nodemat[i][j+1];
                if(i<n-1)  Nodemat[i][j]->down=Nodemat[i+1][j];
             }
+            
         }
         return Nodemat[0][0];
     }
 };
-
 //{ Driver Code Starts.
 
 void display(Node* head) {
