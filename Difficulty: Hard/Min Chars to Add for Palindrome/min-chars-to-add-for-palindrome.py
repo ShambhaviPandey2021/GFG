6fd53567ad9918f57a669ce855217e5309bbdb60
Shambhavi Pandey <112650493+ShambhaviPandey2021@users.noreply.github.com>
2024-12-03 22:34:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Min Chars to Add for Palindrome/min-chars-to-add-for-palindrome.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minChar(self, s):
+        #Write your code here
+        def computeLPS(pattern):
+            lps = [0] * len(pattern)
+            length = 0
+            i = 1
+            
+            while i < len(pattern):
+                if pattern[i] == pattern[length]:
+                    length += 1
+                    lps[i] = length
+                    i += 1
+                else:
+                    if length != 0:
+                        length = lps[length - 1]
+                    else:
+                        lps[i] = 0
+                        i += 1
+            return lps
+        
+        temp = s + '$' + s[::-1]
+        lps = computeLPS(temp)
+        
+        return len(s) - lps[-1]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        s = input()
+        obj = Solution()
+        ans = obj.minChar(s)
+        print(ans)
+        print("~")
+
+# } Driver Code Ends
