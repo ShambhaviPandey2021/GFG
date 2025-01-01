4afc5dfccd1055503d9aa89ded0a0cabfb1492a3
Shambhavi Pandey <112650493+ShambhaviPandey2021@users.noreply.github.com>
2025-01-01 23:20:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print Anagrams Together/print-anagrams-together.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+from collections import defaultdict
+
+class Solution:
+
+    def anagrams(self, arr):
+        '''
+        words: list of word
+        n:      no of words
+        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
+        '''
+
+        #code here
+        agm_dict = defaultdict(list)
+
+        for word in arr:
+            sorted_word = ''.join(sorted(word))
+            agm_dict[sorted_word].append(word)
+
+        return list(agm_dict.values())
+        
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+    t = int(input())
+    for tcs in range(t):
+        words = input().split()
+
+        ob = Solution()
+        ans = ob.anagrams(words)
+
+        for grp in sorted(ans):
+            for word in grp:
+                print(word, end=' ')
+            print()
+
+        print("~")
+
+# } Driver Code Ends
